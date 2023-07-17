336bd788498568fa65e43c3801b1282da75bc791
DaraniShri <daranirameshbabu@gmail.com>
2023-07-17 18:32:13 +0530

    adjust(shop):adjust in modules configuring custom module

---
+++ shop/modules/sp_banner/getContent.tpl
@@ -0,0 +1,9 @@
+<form action="sp_banner.php" method="POST">
+    <div class="content">
+        <label>Module name</label>
+        <input type="text" id="slide-name" name="slide-name">
+    </div>
+    <div>
+        <input type="submit" name="submit" value="save" class="btn btn-primary">
+    </div>
+</form>
--- shop/modules/sp_banner/js/custom.js
+++ shop/modules/sp_banner/js/custom.js
@@ -1,15 +1,16 @@
 $('.multiple-items').slick({
     dots: true,
     infinite: true,
-    speed: 300,
     slidesToShow: 3,
     slidesToScroll: 3,
+    focusOnSelect: true,
+    dots:true,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 34,
+          slidesToScroll: 3,
           infinite: true,
           dots: true
         }
@@ -18,14 +19,8 @@ $('.multiple-items').slick({
         breakpoint: 750,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 360,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 2,
+          dots: true
         }
       }
     ]
